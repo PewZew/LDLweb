aa6305941c8523eef670fa9112fd044c11d01c76
LamHanoi10 <77109479+LamHanoi10@users.noreply.github.com>
2022-06-24 20:40:00 +0700

    Update app.js

--- redirects/invite/app.js
+++ redirects/invite/app.js
@@ -1,5 +1,5 @@
 function redirect() {
-    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=739280940316098610&permissions=536098499710&redirect_uri=https%3A%2F%2Flandonglao.ga%2F&response_type=code&scope=bot%20applications.commands"
+    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=739280940316098610&permissions=536098499710&redirect_uri=https%3A%2F%2Flandonglao.ga%2Fthanks%2F&response_type=code&scope=bot%20applications.commands"
 }
 
 redirect()
